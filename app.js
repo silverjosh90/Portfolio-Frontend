cd323e4f950b97b9552193c24cadfdc3cb168691
Josh Silverstein <silverjosh90@gmail.com>
2016-04-16 19:14:14 -0600

    done besides the CSS overhaul

--- app.js
+++ app.js
@@ -14,4 +14,24 @@ app.config(function($routeProvider){
       templateUrl: 'pages/aboutme.html',
       controller: 'aboutmecontroller'
   })
+    .when('/projects', {
+      templateUrl: 'pages/projects.html',
+      controller: 'projectscontroller'
+  })
+    .when('/smiley-snake', {
+      templateUrl: 'pages/smiley-snake.html',
+      controller: 'projectscontroller'
+  })
+    .when('/sorting-hat', {
+      templateUrl: 'pages/sortinghat.html',
+      controller: 'projectscontroller'
+  })
+    .when('/abetterway', {
+      templateUrl: 'pages/abetterway.html',
+      controller: 'projectscontroller'
+  })
+    .when('/gslack', {
+      templateUrl: 'pages/gslack.html',
+      controller: 'projectscontroller'
+  })
 })
